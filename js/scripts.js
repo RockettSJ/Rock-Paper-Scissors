ee21bb3f1268dc830efaf119fea6c84617b6661b
Steven <stevenrockett@gmail.com>
2020-02-07 00:28:37 +0000

    Interactive version. Work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,142 +1,82 @@
-//Global variables to track score in game().
+const startBtn = document.getElementById("startBtn");
+const instruction = document.getElementById("instruction");
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
 let playerScore = 0;
 let computerScore = 0;
 
+startBtn.addEventListener("click", () => {
+    // todo: remove startBtn when it's clicked
+    createbuttons();
+});
+
+createbuttons = () => {
+    instruction.innerHTML = "Make your choice!";
+    rockBtn.innerHTML = `<img src="images/rock.jpg" />`;
+    paperBtn.innerHTML = `<img src="images/paper.jpg" />`;
+    scissorsBtn.innerHTML = `<img src="images/scissors.jpg" />`;
+};
+
+rockBtn.addEventListener("click", () => {
+    clearButtons();
+    playRound("Rock");
+});
+
+paperBtn.addEventListener("click", () => {
+    clearButtons();
+    playRound("Paper");
+});
+
+scissorsBtn.addEventListener("click", () => {
+    clearButtons();
+    playRound("Scissors");
+});
+
+const clearButtons = () => {
+    rockBtn.innerHTML = "";
+    paperBtn.innerHTML = "";
+    scissorsBtn.innerHTML = "";
+};
+
+playRound = playerChoice => {
+    const computerChoice = computerPlay();
+    //Player = Rock
+    if (playerChoice == "Rock" && computerChoice === 0) {
+        instruction.innerHTML = "It's a tie! You both chose Rock.";
+    } else if (playerChoice == "Rock" && computerChoice === 1) {
+        instruction.innerHTML = "You lose! Paper beats Rock.";
+        computerScore++;
+    } else if (playerChoice == "Rock" && computerChoice === 2) {
+        instruction.innerHTML = "You win! Rock beats Scissors.";
+        playerScore++;
+    }
+
+    //Player = Paper
+    if (playerChoice == "Paper" && computerChoice === 0) {
+        instruction.innerHTML = "You win! Paper beats Rock.";
+        playerScore++;
+    } else if (playerChoice == "Paper" && computerChoice === 1) {
+        instruction.innerHTML = "It's a tie! You both chose Paper.";
+    } else if (playerChoice == "Paper" && computerChoice === 2) {
+        instruction.innerHTML = "You lose! Scissors beats Paper.";
+        computerScore++;
+    }
+
+    //Player = Scissors
+    if (playerChoice == "Scissors" && computerChoice === 0) {
+        instruction.innerHTML = "You lose! Rock beats Scissors.";
+        computerScore++;
+    } else if (playerChoice == "Scissors" && computerChoice === 1) {
+        instruction.innerHTML = "You win! Scissors beats Paper.";
+        playerScore++;
+    } else if (playerChoice == "Scissors" && computerChoice === 2) {
+        instruction.innerHTML = "It's a tie! You both chose Scissors.";
+    }
+};
+
 function computerPlay() {
-    let choices = ["Rock", "Paper", "Scissors"];
-    let randomChoice = Math.floor(Math.random()*choices.length);
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
     return randomChoice;
 }
-
-function playRound() {
-    //Prompt player to make their choice.
-    let playerChoice = window.prompt("Make your choice! Rock, Paper or Scissors?");
-    
-    //Case insensitive comparison of user input. Maybe rewrite to a switch case?
-    if (playerChoice.toLowerCase() == "rock") {
-        playerChoice = "Rock";
-    }
-    else if (playerChoice.toLowerCase() == "paper") {
-        playerChoice = "Paper";
-    }          
-    else if (playerChoice.toLowerCase() == "scissors") {
-        playerChoice = "Scissors";
-    } 
-    else {
-        alert("Please refresh the page and check your spelling!");
-        throw new Error("Please refresh the page and check your spelling!");
-    }     
-
-    let computerChoice = computerPlay();
-    
-    // Relative to number element in choices array. May need reworking (0 = Rock, 1 = Paper, 2 = Scissors)               
-    // Important: = vs == vs ===
-    if (playerChoice == "Rock" && (computerChoice === 0)) {
-        console.log("It's a tie! You both chose Rock.");
-    }               
-    else  if (playerChoice == "Rock" && (computerChoice === 1)) {
-        console.log("You lose! Paper beats Rock.");
-        computerScore++;
-    }
-    else if (playerChoice == "Rock" && (computerChoice === 2)) {
-        console.log("You win! Rock beats Scissors.");
-        playerScore++;
-    }
-
-    if (playerChoice == "Paper" && (computerChoice === 0)) {
-        console.log("You win! Paper beats Rock.");                    
-        playerScore++;
-    }               
-    else  if (playerChoice == "Paper" && (computerChoice === 1)) {
-        console.log("It's a tie! You both chose Paper.");
-    }
-    else if (playerChoice == "Paper" && (computerChoice === 2)) {
-        console.log("You lose! Scissors beats Paper.");
-        computerScore++;
-    }
-
-    if (playerChoice == "Scissors" && (computerChoice === 0)) {
-        console.log("You lose! Rock beats Scissors.");
-        computerScore++;
-    }               
-    else  if (playerChoice == "Scissors" && (computerChoice === 1)) {
-        console.log("You win! Scissors beats Paper.");                    
-        playerScore++;
-    }
-    else if (playerChoice == "Scissors" && (computerChoice === 2)) {
-        console.log("It's a tie! You both chose Scissors.");
-    }
-
-    /*
-    switch(playerChoice) {
-        case 'Rock':
-            switch(computerChoice) {
-                //Relative to number element in choices array. May need reworking (0 = Rock, 1 = Paper, 2 = Scissors)
-                case 0:
-                    console.log("It's a tie! You both chose Rock.");
-                    break;
-                case 1:
-                    console.log("You lose! Paper beats Rock.");
-                    computerScore++;
-                    break;
-                case 2:
-                    console.log("You win! Rock beats Scissors.");
-                    playerScore++;
-                    break;
-            }
-        case 'Paper': 
-            switch(computerChoice) {
-                case 0: 
-                    console.log("You win! Paper beats Rock.");
-                    playerScore++;
-                    break;
-                case 1: 
-                    console.log("It's a tie! You both chose Paper.");
-                    break;
-                case 2: 
-                    console.log("You lose! Scissors beats Paper.");
-                    computerScore++
-                    break;
-            }
-        case 'Scissors': 
-            switch(computerChoice) {
-                case 0: 
-                    console.log("You lose! Rock beats Scissors.");
-                    computerScore++;
-                    break;
-                case 1: 
-                    console.log("You win! Scissors beats Paper.");   
-                    playerScore++;
-                    break;
-                case 2: 
-                    console.log("It's a tie! You both chose Scissors.");  
-                    break;
-            }
-    }
-    */
-}
-
-function game() {
-    const numRounds = 5;
-
-    for (let i = 0; i < numRounds; i++) {
-        playRound();
-        console.log("Your score: " + playerScore);
-        console.log("Computer's score: " + computerScore);
-
-        //Decide the winner of the game
-        if (i == 4) {
-            if (playerScore > computerScore) {
-                console.log("You won the game!");
-            }
-            else if (playerScore < computerScore) {
-                console.log("You lost the game!");
-            }
-            else {
-                console.log("It's a tie!");
-            }
-        }
-    }
-}  
-
-game();
