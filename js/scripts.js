a88c80b7d1b3accb73df675abecd4f4a9e1fb1e7
Steven <stevenrockett@gmail.com>
2020-02-07 14:17:12 +0000

    computerPlay() const changed to let

--- js/scripts.js
+++ js/scripts.js
@@ -119,6 +119,6 @@ displayInfo = () => {
 
 function computerPlay() {
     const choices = ["Rock", "Paper", "Scissors"];
-    const randomChoice = Math.floor(Math.random() * choices.length);
+    let randomChoice = Math.floor(Math.random() * choices.length);
     return randomChoice;
 }
