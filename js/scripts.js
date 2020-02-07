4ea3ed22673fb496288ed07bac478ef292d1480b
Steven <stevenrockett@gmail.com>
2020-02-07 14:13:39 +0000

    Almost finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
 const startBtn = document.getElementById("startBtn");
 const instruction = document.getElementById("instruction");
-const rockBtn = document.getElementById("rockBtn");
-const paperBtn = document.getElementById("paperBtn");
-const scissorsBtn = document.getElementById("scissorsBtn");
+const gameInfo = document.getElementById("gameInfo");
+const roundDisplay = document.getElementById("rounds");
+let numRounds = 1;
 let playerScore = 0;
 let computerScore = 0;
 
 startBtn.addEventListener("click", () => {
-    // todo: remove startBtn when it's clicked
-    createbuttons();
+    createInterface();
 });
 
-createbuttons = () => {
+createInterface = () => {
+    const rockBtn = document.getElementById("rockBtn");
+    const paperBtn = document.getElementById("paperBtn");
+    const scissorsBtn = document.getElementById("scissorsBtn");
+    roundDisplay.innerHTML = `Round ${numRounds}`;
     instruction.innerHTML = "Make your choice!";
     rockBtn.innerHTML = `<img src="images/rock.jpg" />`;
     paperBtn.innerHTML = `<img src="images/paper.jpg" />`;
     scissorsBtn.innerHTML = `<img src="images/scissors.jpg" />`;
+    startBtn.style.display = "none";
+    numRounds++;
 };
 
 rockBtn.addEventListener("click", () => {
@@ -40,6 +45,7 @@ const clearButtons = () => {
 };
 
 playRound = playerChoice => {
+    //todo: make images of choices and outcome (paper hugging rock, rock punching scissors with K.O, scissors giving paper a haircut)
     const computerChoice = computerPlay();
     //Player = Rock
     if (playerChoice == "Rock" && computerChoice === 0) {
@@ -73,6 +79,42 @@ playRound = playerChoice => {
     } else if (playerChoice == "Scissors" && computerChoice === 2) {
         instruction.innerHTML = "It's a tie! You both chose Scissors.";
     }
+
+    displayInfo();
+    checkRounds();
+    //Give user time to read result, then continue
+    setTimeout(function() {
+        nextRound();
+    }, 1500);
+};
+
+checkRounds = () => {
+    if (numRounds == 6) {
+        if (playerScore > computerScore) {
+            instruction.innerHTML = "You won the game!";
+        } else if (computerScore > playerScore) {
+            instruction.innerHTML = "You lost the game!";
+        } else {
+            instruction.innerHTML = "It's a tie!";
+        }
+        roundDisplay.innerHTML = "Game over!";
+
+        // Prevent game from continuing after 5 rounds
+        window.playRound() = function() {
+            return false;
+        };
+
+        numRounds = 0;
+    }
+};
+
+nextRound = () => {
+    createInterface();
+};
+
+displayInfo = () => {
+    gameInfo.innerHTML = `
+    You: ${playerScore} - CPU: ${computerScore}`;
 };
 
 function computerPlay() {
